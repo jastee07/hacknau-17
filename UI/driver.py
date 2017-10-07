292b73a4bd3b8e5c5434b3d419a652fe3611a9eb
Jake Steele <jake.steele54@gmail.com>
2017-10-07 18:48:58 -0500

    Added calibration methods and run methods

--- UI/driver.py
+++ UI/driver.py
@@ -3,33 +3,39 @@ from Tkinter import *
 
 class Application(Frame):
     calibrating = False
+    running = False
+
     def start_calibration(self):
-        self.calibrate.config(text="Quit Calibration", command=self.end_calibration)
+        self.calibrate.config(text="Quit Calibrating", command=self.end_calibration)
 
     def end_calibration(self):
         self.calibrate.config(text="Calibrate", command=self.start_calibration)
 
     def start_run(self):
-        pass
+        self.run.config(text="Quit Running", command=self.end_run)
 
     def end_run(self):
-        pass
+        self.run.config(text="Run", command=self.start_run)
 
-    def createWidgets(self):
+    def create_widgets(self):
+        button_height = 2
+        button_width = 14
         self.calibrate = Button(self)
         self.calibrate["text"] = "Calibrate"
         self.calibrate["command"] = self.start_calibration
         self.calibrate.pack({"side": "left"})
+        self.calibrate.config(height=button_height, width=button_width)
 
         self.run = Button(self)
         self.run["text"] = "Run"
-        self.calibrate["command"] = self.start_run
+        self.run["command"] = self.start_run
         self.run.pack({"side": "right"})
+        self.run.config(height=button_height, width=button_width)
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.pack()
-        self.createWidgets()
+        self.create_widgets()
 
 root = Tk()
 app = Application(master=root)
