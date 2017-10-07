64335bf85039b03638249cd5c097ac058e5bca56
Jake Steele <jake.steele54@gmail.com>
2017-10-07 18:29:53 -0500

    Added calibration methods and run methods

--- UI/driver.py
+++ UI/driver.py
@@ -1,14 +1,15 @@
 from Tkinter import *
 
-class Application(Frame):
 
-    def calibration(self):
-        pass
+class Application(Frame):
+    calibrating = False
+    def start_calibration(self):
+        self.calibrate.config(text="Quit Calibration", command=self.end_calibration)
 
     def end_calibration(self):
-        pass
+        self.calibrate.config(text="Calibrate", command=self.start_calibration)
 
-    def run(self):
+    def start_run(self):
         pass
 
     def end_run(self):
@@ -16,10 +17,14 @@ class Application(Frame):
 
     def createWidgets(self):
         self.calibrate = Button(self)
-        self.calibrate["text"] = "Calibrate",
-        self.calibrate["command"] = self.calibration
-
+        self.calibrate["text"] = "Calibrate"
+        self.calibrate["command"] = self.start_calibration
         self.calibrate.pack({"side": "left"})
+
+        self.run = Button(self)
+        self.run["text"] = "Run"
+        self.calibrate["command"] = self.start_run
+        self.run.pack({"side": "right"})
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
